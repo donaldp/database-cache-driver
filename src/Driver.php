6aa05e6edd96e94d6aabed5b49ae753634c7a46d
Donald Pakkies <donald@sov.tech>
2019-11-03 16:51:31 +0200

    feat: initialize the driver

--- src/Driver.php
+++ src/Driver.php
@@ -7,5 +7,13 @@ use Modulus\Hibernate\Cache\DriverInterface;
 
 class Driver extends Base implements DriverInterface
 {
-  //
+  /**
+   * __construct
+   *
+   * @return void
+   */
+  public function __construct()
+  {
+    parent::__construct();
+  }
 }
