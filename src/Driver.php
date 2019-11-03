d52a2bb7b9ccd25fa69c21f27e9010f4bec68c04
Donald Pakkies <donald@sov.tech>
2019-11-03 16:50:52 +0200

    feat: created a cache driver

---
+++ src/Driver.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Don47\DatabaseCacheDriver;
+
+use Modulus\Hibernate\Cache\Driver as Base;
+use Modulus\Hibernate\Cache\DriverInterface;
+
+class Driver extends Base implements DriverInterface
+{
+  //
+}
